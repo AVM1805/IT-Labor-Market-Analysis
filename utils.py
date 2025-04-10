12608d8d48482c938cb6d877c76136fe1896ac86
Alexandr <m9502490764@gmail.com>
2025-04-10 13:05:14 +0200

    only vacancies where skills specified considered

--- utils.py
+++ utils.py
@@ -30,15 +30,46 @@ def clean_up(jobs:pd.DataFrame, settings, save_as_new_file = False, save_to = No
         jobs.loc[i, "description"] = str(jobs.loc[i, "description"]).replace("About the job", "").strip().lower()
         jobs.loc[i, "skills"] = str(jobs.loc[i, "skills"]).replace(";", ",").strip().lower()
     jobs.drop(labels=list(jobs_to_exclude), axis="index", inplace=True)
-    jobs.reset_index(inplace=True)
+    jobs.reset_index(inplace=True, drop=True)
     if save_as_new_file:
         jobs.to_csv(save_to, sep=',', index=False)
 
     return jobs
-    
+
+def check_skill_in_vacancies(name, *fields):
+    for field in fields:
+        try:
+            index = str(field).index(name)
+            if index+len(name) >= len(field):
+                continue
+            next_character = field[index+len(name)]
+            if (" "+name in field and not next_character.isalpha()) or ", "+name+"," in field or  ","+name+"," in field:
+                return True
+        except ValueError:
+            continue
+    return False
+
+def filter_vacancies_accroding_skills_occurences(jobs:pd.DataFrame, settings):
+    indices_to_keep = set()
+    for i in range(len(jobs)):
+        _b = False
+        for skill in settings["skills"]: 
+            for name in skill:
+                if check_skill_in_vacancies(name, jobs.loc[i, "description"], jobs.loc[i, "skills"]):
+                    indices_to_keep.add(i)
+                    _b = True
+                    break
+            if _b:
+                break
+
+    res = jobs.loc[list(indices_to_keep)]
+    res.reset_index(inplace=True, drop=True)
+    return res
+
 if __name__ == "__main__":
     settings_path = "./settings.json"
     jobs = pd.read_csv("./jobs.csv")
     settings = read_settings_from_file(settings_path)
     jobs = clean_up(jobs, settings)
+    jobs = filter_vacancies_accroding_skills_occurences(jobs, settings)
     print(jobs)
